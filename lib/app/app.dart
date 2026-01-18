27abe6ecae958b830a3d4f29e3c336cc580f8d58
MD shakil Hossain <mdshakilhossain720@gmail.com>
2026-01-18 22:46:10 +0600

    app

---
+++ lib/app/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  //const MyApp({super.key});
+  MyApp._internal();
+
+  static final instance = MyApp._internal();
+  factory MyApp() => instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
